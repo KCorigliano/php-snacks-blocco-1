05eed8f37babd1e79e1273d485c05f5f864c8336
KCorigliano <corigliano98@hotmail.com>
2022-02-15 17:17:12 +0100

    cicle created

---
+++ snack-4/snack-4.php
@@ -0,0 +1,16 @@
+<?php
+
+$randomNum = [];
+
+while (count($randomNum)<16) { 
+    $num = rand(1, 100);
+    if (!in_array($num, $randomNum)) {
+        $randomNum[] += $num;
+    }
+};
+
+foreach ($randomNum as $key => $result) {
+    echo "<p><span style=font-weight:bold>-$key</span> $result </p>";
+}
+
+?>
